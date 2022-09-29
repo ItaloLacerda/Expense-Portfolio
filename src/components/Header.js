154c55430916c1084a7a0fddd0fcbf0e84a08152
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-09-29 20:11:03 -0300

    [REQ-02] create header and add wallet page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { email, idToEdit } = this.props;
     return (
-      <div>Header</div>
+      <>
+        <h4 data-testid="email-field">{ `Email: ${email}` }</h4>
+        <h4 data-testid="total-field">{ `Despesa Total: ${idToEdit}` }</h4>
+        <h4 data-testid="header-currency-field">BRL</h4>
+      </>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = ({ user, wallet }) => ({
+  email: user.email,
+  idToEdit: wallet.idToEdit,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  idToEdit: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
