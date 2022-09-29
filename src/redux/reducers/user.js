39145415693b02f43c1f1dd38868bcc32123ecbe
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-09-29 15:26:23 -0300

    [REQ-01] initial configuration of redux and routes

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,8 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+const userReducer = (state = INITIAL_STATE) => state;
+
+export default userReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,9 @@
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
 // configure aqui sua store
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export default store;
