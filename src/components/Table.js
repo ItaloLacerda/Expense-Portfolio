7520cfef0fde445d38d1b30445e58612bf5cd54d
Italo Lacerda <italo.lacerda2017@hotmail.com>
2022-10-01 05:20:19 -0300

    [REQ-07] Implement logic so that the table is fed by application state

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expense } = this.props;
     return (
       <table>
         <tr>
@@ -15,9 +18,35 @@ class Table extends Component {
           <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </tr>
+        <tbody>
+          {expense.map((element) => {
+            const { id, description, tag, method, value,
+              exchangeRates, currency } = element;
+            return (
+              <tr key={ id }>
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>{Number(value).toFixed(2)}</td>
+                <td>{exchangeRates[currency].name}</td>
+                <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
+                <td>{Number(exchangeRates[currency].ask * value).toFixed(2)}</td>
+                <td>BRL</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = ({ wallet }) => ({
+  expense: wallet.expenses,
+});
+
+Table.propTypes = {
+  expense: PropTypes.arrayOf.isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
